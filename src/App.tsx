1bad112837117eaeabc7c239313018c8bc3cff8c
venushaamarathunga <avenusha95@gmail.com>
2026-03-27 13:33:49 +0200

    Add todo list changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,41 @@
-const App = () => {
-  
-  const handleAddTodo =() =>{
+import { useState } from "react";
+import "./style.model.css";
 
+interface Todo {
+  message: string;
+  id: number;
+}
+
+const App = () => {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAddTodo = () => {
+    const todoObj: Todo = { message: todo, id: Date.now() };
+
+    setTodos((prev: Todo[]) => [todoObj, ...prev]);
+    setTodo("");
+  };
+
+  const handleDelete = (id: number) => {
+    const filterdTodos = todos.filter(todo => todo.id !== id)
+    setTodos(filterdTodos)
   }
 
-  return(
-  <div>
-   <input type="text" name='todo' placeholder="add the todos..." />
-   <button onClick={handleAddTodo}>Add</button>
-  </div>
-  )
-
-}
+  return (
+    <div className="container">
+      <div className="searchContainer">
+        <input className="input" type="text" value={todo} onChange={(e) => setTodo(e.target.value)} name="todo" placeholder="add the todos..." />
+        <button className="btn" onClick={handleAddTodo}>
+          Add
+        </button>
+      </div>
+      <div className="todoList" >{todos.map((todo) => {
+        return <button key={todo.id} onClick={() => handleDelete(todo.id)} className="todoBtn">{todo.message}</button>
+      })}
+      </div>
+    </div>
+  );
+};
 
 export default App;
