39593674d8cfbf89a123961a6fc88808c10a683c
venushaamarathunga <avenusha95@gmail.com>
2026-03-27 12:14:31 +0200

    add todo application

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+const App = () => {
+  
+  const handleAddTodo =() =>{
+
+  }
+
+  return(
+  <div>
+   <input type="text" name='todo' placeholder="add the todos..." />
+   <button onClick={handleAddTodo}>Add</button>
+  </div>
+  )
+
+}
+
+export default App;
